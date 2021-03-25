4fa2259d5f6f31ac0e5cba92a7023791cf5a7606
90felipe09 <felhipeshiraishi@gmail.com>
2021-03-25 19:15:08 -0300

    setup: ep4 start project

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to ep4-logica-de-programacao
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ src/ep4_logica_de_programacao/core.clj
@@ -0,0 +1,7 @@
+(ns ep4-logica-de-programacao.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
